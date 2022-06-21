ef05193c35f334a2d3318519d54b9eb4a50d74b1
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-06-21 14:01:19 +0530

    Commit 2

--- src/test/java/DevClassTest.java
+++ src/test/java/DevClassTest.java
@@ -1,5 +1,10 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class DevClassTest {
     public static void main(String[] args) {
         System.out.println("DevClassTest");
+        System.out.println("Omkar Methods");
     }
+
+    //Omkar methods
 }
